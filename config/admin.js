b13052e044729aff40cb6669e749697ac4325f7e
elecompu <elecompu@126.com>
2023-01-06 16:52:04 +0800

    again;

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','d46vfSOU8IxsiAE54O4QNA=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','d46vfSOU8IxsiAE54O4QNAxx'),
   },
 });
