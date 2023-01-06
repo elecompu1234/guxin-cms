f85ba0b8d81e7d6509b8b8ced763a5176005c6dc
elecompu <elecompu@126.com>
2023-01-06 15:30:56 +0800

    change user name.

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '334466520'),
     },
       ssl: {
