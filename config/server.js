6f77265f94d59e0a75bda07337861c1d494501eb
elecompu <elecompu@126.com>
2023-01-06 16:05:48 +0800

    again;

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('334466520aa'),
+    keys: env.array(['myKeyA', 'myKeyB']),
   },
 });
